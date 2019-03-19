083571c0c06e4e7609faa738e585d86496a2b170
Anders <kiss7medical@gmail.com>
2019-03-19 19:40:22 +0100

    create and update actions added to wishlists_controller.rb

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,14 +4,34 @@ class WishlistsController < ApplicationController
   end
 
   def new
+    @user = current_user
+    @wishlist = Wishlist.new
   end
 
   def create
+    @wishlist = Wishlist.new(wishlist_params)
+    @wishlist.user = current_user
+
+    # double-check 
+    render 'new' if @wishlist.save
   end
 
   def edit
   end
 
   def update
+    if @instrument.update(instrument_params)
+      redirect_to @instrument
+    else
+      # double-check 
+      render 'edit'
+    end
   end
+
+  private
+
+  def wishlist_params
+    params.require(:wishlist).permit(:preference)
+  end
+
 end
