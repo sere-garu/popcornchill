8615d2d694017695b6432dcfe59d5162eb77dad6
Anders <kiss7medical@gmail.com>
2019-03-19 17:48:15 +0100

    create wishlists_controller.rb

---
+++ app/controllers/wishlists_controller.rb
@@ -0,0 +1,17 @@
+class WishlistsController < ApplicationController
+  def index
+    @wishlists = Wishlist.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
