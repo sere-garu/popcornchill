6de773455424dcd4e03a8d4af4ffd6dc34e89d47
serena garuti <sere.garuti@gmail.com>
2019-03-19 19:20:31 +0100

    Insert email to create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[]
+  before_action :set_event, only: %i[show edit update destroy]
 
   def index
     @events = Event.all
@@ -14,6 +14,21 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      UserEvent.create(user: current_user, event: @event, status: "admin")
+
+      params[:emails].each do |email|
+        password = SecureRandom.hex(10)
+        user = User.create!(email: email, password: password, password_confirmation: password)
+        UserEvent.create(user: user, event: @event, status: "pending") if user
+      end
+
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,6 +38,8 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    @event.destroy
+    redirect_to root_path
   end
 
   private
@@ -31,5 +48,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-
+  def event_params
+    params.require(:event).permit(:name, :address, :date)
+  end
 end
