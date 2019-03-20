043c664e840e586ae08cdc660e7a0b40903d0b6e
Oscar Bernardini <bernardini687@gmail.com>
2019-03-20 12:16:29 +0100

    Initials devise tweaks

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: :registrations }
 
   resources :events , except: :index do
     resources :results, only: %i[index create]
