5be2d804d30f8d61c52db5e6c09efc2baaa8b20b
Oscar Bernardini <bernardini687@gmail.com>
2019-03-21 20:50:01 +0100

    Checkpoint

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,6 +2,18 @@ class ResultsController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
     @results = @event.results.reject { |r| r.preference == 'nope' }
+
+    user_wishlists = @event.users.map do |user|
+      a = user.wishlists.pluck :movie_id
+      b = user.wishlists.pluck :preference
+      a.map do |e|
+        [e, b[a.index(e)]]
+      end
+    end.flatten.each_slice(2).to_a
+
+    c = user_wishlists.select { |e| user_wishlists.count(e) > 1 }.uniq
+    # try to have only yeps
+    # use the resulting index to find that movie and push it in results
     raise
   end
 
@@ -17,6 +29,8 @@ class ResultsController < ApplicationController
   end
 
   private
+
+  def set_event; end
 
   def result_params
     params.permit(:preference, :movie_id, :user_id, :event_id)
