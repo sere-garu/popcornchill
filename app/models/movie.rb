a0fe6c2f8c6183809a9a7a54e4611ce219ce4b31
Oscar Bernardini <bernardini687@gmail.com>
2019-03-21 12:00:44 +0100

    Wishlists can be set and edited

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,9 @@ class Movie < ApplicationRecord
   has_many :wishlists
   has_many :users, through: :wishlists
 
-
   validates :trakt_payload, presence: true
 
+  def preference?(user)
+    user.wishlists.pluck(:movie_id).include? id
+  end
 end
