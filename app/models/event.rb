4e26942bd333f56f2ed920fdc00176c98975a359
Oscar Bernardini <bernardini687@gmail.com>
2019-03-24 10:56:52 +0100

    Add partials for events#index

--- app/models/event.rb
+++ app/models/event.rb
@@ -25,15 +25,13 @@ class Event < ApplicationRecord
     user_events.pluck(:status).count('accepted') == @accepted_users - 1
   end
 
-  def everyone_pending?
-    user_events.map(&:status).uniq.sort == %w[admin pending]
-  end
-
-  def admin?(user)
-    user_events.where(status: :admin).take.user.name == user.name
-  end
-
   def user_already_swiped(user, movie)
     results.where(user: user, movie: movie).any?
+  end
+
+  # Not in use
+
+  def everyone_pending?
+    user_events.map(&:status).uniq.sort == %w[admin pending]
   end
 end
